be666e370d6ee635eccff55e294ca51e6e84d6bc
yanaleenpluas <85530151+yanaleenpluas@users.noreply.github.com>
2023-02-01 04:08:24 -0500

    Proyecto4
    Proyecto4 completo

--- Proyecto4/admin/routes/index.js
+++ Proyecto4/admin/routes/index.js
@@ -22,12 +22,47 @@ router.get('/customer', async function(req, res, next) {
 })
 
 /* GET home CUSTOMER ORDER. */
-router.get('/customer/orders/:customerNumber', async function(req, res, next) {
+router.get('/customer/orders/:customerNumber',  function(req, res, next) {
   const URL = `https://api-2-5a450-default-rtdb.firebaseio.com/collection.json?orderBy=%22customerNumber%22&equalTo=${req.params.customerNumber}`
+  const response =  axios.get(URL);
+  response.then((data) => {
+    let shippedArray = [];
+    for (let key in data.data){
+      if(data.data[key].status === 'Shipped'){
+        shippedArray.push(data.data[key]);
+      }
+    }
+    res.render('order', { title: 'Order', orderShipper: shippedArray , id:req.params.customerNumber });
 
-  const response = await axios.get(URL) 
-  res.json(response.data)
 
-})
+  });
+});
+/* GET home CUSTOMER productos. */
+router.get('/customer/productos/:customerNumber',  function(req, res, next) {
+  const URL = `https://api-2-5a450-default-rtdb.firebaseio.com/collection.json?orderBy=%22customerNumber%22&equalTo=${req.params.customerNumber}`
+  const response =  axios.get(URL);
+  response.then((data) => {
+    let productosArray = [];
+    for (let key in data.data){
+        productosArray.push(data.data[key]);
+    }
+    res.render('productos', { title: 'Productos', productos: productosArray , id:req.params.customerNumber });
+  });
+});
+
+
+router.get('/productosALL', async function(req, res, next) {
+  const URL = `https://api-2-5a450-default-rtdb.firebaseio.com/collection.json`
+
+  const response =  axios.get(URL);
+  response.then((data) => {
+    let productosArray = [];
+    for (let key in data.data){
+        productosArray.push(data.data[key]);
+    }
+    res.render('productosALL', { title: 'Productos', productos: productosArray , id:req.params.customerNumber });
+  });
+
+});
 
 module.exports = router;
