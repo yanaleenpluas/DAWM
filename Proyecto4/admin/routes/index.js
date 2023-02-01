98fe62fa7cb57aa5332f3d3c0db7fea736103323
yanaleenpluas <85530151+yanaleenpluas@users.noreply.github.com>
2023-02-01 08:10:58 -0500

    add
    add

--- Proyecto4/admin/routes/index.js
+++ Proyecto4/admin/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const axios = require('axios')
 var router = express.Router();
+let arrayship = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -32,11 +33,11 @@ router.get('/customer/orders/:customerNumber',  function(req, res, next) {
         shippedArray.push(data.data[key]);
       }
     }
+    arrayship=shippedArray;
     res.render('order', { title: 'Order', orderShipper: shippedArray , id:req.params.customerNumber });
-
-
   });
 });
+
 /* GET home CUSTOMER productos. */
 router.get('/customer/productos/:customerNumber',  function(req, res, next) {
   const URL = `https://api-2-5a450-default-rtdb.firebaseio.com/collection.json?orderBy=%22customerNumber%22&equalTo=${req.params.customerNumber}`
@@ -44,12 +45,23 @@ router.get('/customer/productos/:customerNumber',  function(req, res, next) {
   response.then((data) => {
     let productosArray = [];
     for (let key in data.data){
+      let bool=true;
+      productosArray.forEach((item)=>{
+        //pushes only unique element
+          if(item.productCode===data.data[key].productCode){
+            bool = false;
+        }
+      })
+      if(bool){
         productosArray.push(data.data[key]);
+      }
+
     }
     res.render('productos', { title: 'Productos', productos: productosArray , id:req.params.customerNumber });
   });
 });
 
+/* GET home PRODUCTOS ALL. */
 
 router.get('/productosALL', async function(req, res, next) {
   const URL = `https://api-2-5a450-default-rtdb.firebaseio.com/collection.json`
@@ -58,11 +70,39 @@ router.get('/productosALL', async function(req, res, next) {
   response.then((data) => {
     let productosArray = [];
     for (let key in data.data){
+      let bool=true;
+      productosArray.forEach((item)=>{
+        //pushes only unique element
+          if(item.productCode===data.data[key].productCode){
+            bool = false;
+        }
+      })
+      if(bool){
         productosArray.push(data.data[key]);
+      }
+
     }
     res.render('productosALL', { title: 'Productos', productos: productosArray , id:req.params.customerNumber });
   });
 
+
+});
+
+/* POST API AGREGADA  */
+router.post('/order/total/', async function(req, res, next) {  
+  const URL = `https://api-2-5a450-default-rtdb.firebaseio.com/collection.json`
+  const config = {
+    proxy: {
+      host: 'localhost',
+      port: 3000
+    }
+  }  
+  const response = await axios.put(URL, arrayship, config);
+  if(response.status == '200' && response.statusText == 'OK') {
+    res.redirect('/photos')
+  } else {
+    res.redirect('/') 
+  }
 });
 
 module.exports = router;
